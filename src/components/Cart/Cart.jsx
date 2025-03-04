d20bb2ade16c21eeb7e892d56560edc2cb67f87b
parthi-n <parthiban.n@live.com>
2025-03-05 03:16:48 +0800

    updated add to cart, add quantity, remove quantity, remove item

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router";
+import CartItem from "../CartItem/CartItem";
+import { UserContext } from "../../contexts/userContext";
+import { queueNumServices } from "../../services/queueServices";
+import { checkoutServices } from "../../services/checkoutServices";
 import "./Cart.css";
 
-const Cart = ({ cart, removeFromCart }) => {
+const Cart = ({ cart, handleRemoveItem, handleAddQuantity, handleRemoveQuantity, totalPrice }) => {
 	const navigate = useNavigate();
-	const handleCheckout = () => {
-		navigate("/checkout");
+	const { user } = useContext(UserContext);
+	const userID = user._id;
+	console.log("test " + user._id);
+
+	const [updatedCart, setUpdatedCart] = useState([]);
+
+	const handleCheckout = async () => {
+		try {
+			// const queueNum = await queueNumServices();
+			//console.log(queueNum);
+			const checkoutData = {
+				orders: updatedCart,
+				totalPrice: totalPrice,
+				queueNumber: 1,
+				status: "Preparing",
+			};
+			console.log(checkoutData);
+			const response = await checkoutServices(userID, checkoutData);
+
+			console.log(response);
+			navigate("/checkout");
+		} catch (err) {
+			// Handle errors (e.g., display message to the user)
+			console.error("Checkout error:", err.message);
+		}
 	};
+
+	useEffect(() => {
+		setUpdatedCart([...cart]);
+		console.log(updatedCart);
+	}, [cart]);
 
 	return (
 		<div className="page-wrapper">
@@ -15,27 +47,30 @@ const Cart = ({ cart, removeFromCart }) => {
 				<p>Your cart is empty.</p>
 			) : (
 				<div className="menu-wrapper">
-					{cart.map((item, index) => (
-						<div key={item._id} className="menu-card">
-							<div>
-								<p className="item-name">
-									<strong>{item.itemName}</strong>
-								</p>
-								<p className="item-description">{item.description}</p>
-								<p className="item-price">$ {item.price}</p>
-
-								<button className="item-button" onClick={() => removeFromCart(index)}>
-									Remove
-								</button>
-							</div>
-							<img className="item-img" src={item.image} alt={item.itemName} />
-						</div>
+					{updatedCart.map((item, index) => (
+						<CartItem
+							key={item._id}
+							cartItem={item}
+							handleAddQuantity={handleAddQuantity}
+							handleRemoveQuantity={handleRemoveQuantity}
+							handleRemoveItem={handleRemoveItem}
+						/>
 					))}
 				</div>
 			)}
-			<button className="checkout-button" onClick={handleCheckout}>
-				Checkout
-			</button>
+
+			<p>
+				Total: <b>$ {totalPrice}</b>
+			</p>
+			{cart.length > 0 ? (
+				<button className="checkout-button" onClick={handleCheckout}>
+					Checkout
+				</button>
+			) : (
+				<a href="/">
+					<button className="checkout-button">Order Food</button>
+				</a>
+			)}
 		</div>
 	);
 };
---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import "./MenuItem.css";
+
+export default function MenuItem({ menuItem, addToCart, cart, handleAddQuantity, handleRemoveQuantity }) {
+	// State to track if the menuItem is in the cart
+	const [isItemInCart, setIsItemInCart] = useState(false);
+	const [itemInCart, setItemInCart] = useState({});
+
+	// useEffect to update the state when the cart changes
+	useEffect(() => {
+		// Ensure cart is always an array to avoid errors
+		if (!cart || cart.length === 0) {
+			setIsItemInCart(false); // Set to false if cart is empty or undefined
+		} else {
+			const itemInCart = cart.some((item) => item._id === menuItem._id);
+			setIsItemInCart(itemInCart); // Update the state when cart changes
+
+			const checkItem = cart.find((item) => item._id === menuItem._id);
+			setItemInCart(checkItem);
+		}
+	}, [cart]);
+
+	return (
+		<div className="item-card">
+			<div>
+				<p className="item-name">
+					<strong>{menuItem.itemName}</strong>
+				</p>
+				<p className="item-description">{menuItem.description}</p>
+				<p className="item-price">$ {menuItem.price}</p>
+				{isItemInCart ? (
+					<div className="">
+						<div className="">
+							<button type="button" onClick={() => handleRemoveQuantity(menuItem._id)}>
+								-
+							</button>
+							<div>
+								<span className="mx-3 font-bold">{itemInCart.quantity}</span>
+							</div>
+
+							<button type="button" onClick={() => handleAddQuantity(menuItem._id)}>
+								+
+							</button>
+						</div>
+					</div>
+				) : (
+					<button className="item-button" onClick={() => addToCart(menuItem)}>
+						Add to Cart
+					</button>
+				)}
+			</div>
+			<img className="item-img" src={menuItem.image} alt={menuItem.itemName} />
+		</div>
+	);
+}
